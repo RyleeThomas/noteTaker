51394ec2adc6f540bfb3e15b9b84ed39eb85ab73
rylee thomas <ryleetreat@gmail.com>
2022-02-07 21:47:18 -0500

    update get routes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,13 +1,32 @@
-const {notes} = require ('../../db/notes');
-
 const router = require('express').Router();
+const notes = require ("../../db/notes.json");
+const fs = require('fs');
+const path = require('path');
+
+function createNewNote (body, noteArray) {
+    const note = body;
+    noteArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/notes.json'),
+        JSON.stringify({noteArray}, null, 2)
+    )
+    return note;
+}
 
 router.get('/notes', (req, res) => {
-    res.json(notes);
-});
+    let results = notes;
+    res.json(results);
+})
 
-//by id
+router.post('/notes', (req, res) => {
+    //set id based on what the next index of the array will be
+    req.body.id = 1;
 
-//add
+    //add note to json file 
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
+})
 
 //destroy
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const {notes} = require('./db/notes');
 const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require('./routes/apiRoutes/notesRoutes');
+const htmlRoutes = require('./routes/htmlRoutes/index');
 
 const app = express();
 
@@ -11,6 +13,10 @@ app.use(express.json());
 
 //instruscts the server to make certain files readily available
 app.use(express.static('public'));
+
+//routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
