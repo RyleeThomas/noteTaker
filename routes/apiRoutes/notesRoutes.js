4db2b5914ccaf257a49582ebb01d52d00071f326
rylee thomas <ryleetreat@gmail.com>
2022-02-08 00:14:22 -0500

    fixed push bud

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -3,15 +3,8 @@ const notes = require ("../../db/notes.json");
 const fs = require('fs');
 const path = require('path');
 
-function createNewNote (body, noteArray) {
-    const note = body;
-    noteArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, '../../db/notes.json'),
-        JSON.stringify({noteArray}, null, 2)
-    )
-    return note;
-}
+//unique id name
+const uniqid = require('uniqid');
 
 router.get('/notes', (req, res) => {
     let results = notes;
@@ -19,12 +12,24 @@ router.get('/notes', (req, res) => {
 })
 
 router.post('/notes', (req, res) => {
-    //set id based on what the next index of the array will be
-    req.body.id = 1;
+    req.body.id = uniqid.time();
+    console.log('req.body:', req.body)
 
-    //add note to json file 
-    const newNote = createNewNote(req.body, notes);
-    res.json(newNote);
+    const note = req.body
+    
+    console.log('note:', note)
+
+    notes.push(note);
+
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'), 
+        JSON.stringify(notes)
+    )
+    
+    if(res) {
+        console.log(`Post successful!`)
+        return res.json()
+    }
 })
 
 //destroy
